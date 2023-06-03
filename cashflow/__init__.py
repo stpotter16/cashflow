2623e439a25925ff02a69c5a140eb5a00e7c9b96
Sam Potter <spotter1642@gmail.com>
2023-06-03 13:39:32 -0500

    Add basic app factory

--- cashflow/__init__.py
+++ cashflow/__init__.py
@@ -1,1 +1,31 @@
+import os
+
+from flask import Flask
+
 __version__ = "0.1.0"
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'cashflow.sqlite'),
+        )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    return app
+
+
+
