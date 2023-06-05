6adcba097d901cb881cbd639d9472651364ad853
Sam Potter <spotter1642@gmail.com>
2023-06-04 19:46:57 -0500

    Add flake8 tooling

--- noxfile.py
+++ noxfile.py
@@ -4,3 +4,11 @@ import nox
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
+
+locations = "cashflow", "tests", "noxfile.py"
+
+@nox.session(python=["3.10", "3.9", "3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
