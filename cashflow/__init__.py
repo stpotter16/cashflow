7e49371e978f9c4c2253f6a515f3769f7f3c98e9
Sam Potter <spotter1642@gmail.com>
2023-06-27 08:33:17 -0500

    Add annotations to application factory

--- cashflow/__init__.py
+++ cashflow/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 __version__ = "0.1.0"
 
 
-def create_app(test_config=None):
+def create_app(test_config=None) -> Flask:
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
