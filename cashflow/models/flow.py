f250209a8c8a2b67fc866465aa42e8a5f9e2134f
Sam Potter <spotter1642@gmail.com>
2023-10-03 04:49:43 -0500

    Add model for flow

---
+++ cashflow/models/flow.py
@@ -0,0 +1,13 @@
+"""Database model for cashflow."""
+from cashflow import db
+
+
+class Flow(db.Model):
+    """Flow data model.
+
+    Represents the database schema for a flow.
+    """
+
+    id = db.Column(db.String, primary_key=True)
+    created = db.Column(db.DateTime)
+    name = db.Column(db.String, nullable=True)
